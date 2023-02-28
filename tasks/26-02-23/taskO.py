319f2df6ce3c7119a327e383bf2793251f1ac786
AndreyPuchinin <78998186+AndreyPuchinin@users.noreply.github.com>
2023-02-28 09:57:21 -0800

    Update taskO.py

--- tasks/26-02-23/taskO.py
+++ tasks/26-02-23/taskO.py
@@ -1,23 +1,43 @@
-class TaskOMetaClass:
-    def second_max(self):
-        v = self.get_input()
-        if v == 0:
-            return (0, 0)
-        res = self.second_max()
-        if v >= res[0]:
-            return (v, res[0])
-        if v >= res[1]:
-            return (res[0], v)
-        return res
+def get_input():
+    return input("enter a number: ")
 
+
+def second_max(get_input_f):
+    v = int(get_input_f())
+    # print('one')
+    if v == 0:
+        return 0, 0
+    res = second_max(get_input_f)
+    # print('two')
+    if v >= res[0]:
+        return v, res[0]
+    if v >= res[1]:
+        return res[0], v
+    return res
+
+
+class TaskOClass1:
+    def __init__(self, els):
+        self.els = els
+        self.n = 0
+
+    def get_input(self):
+        self.n += 1
+        if self.n <= len(self.els):
+            return self.els[self.n-1]
+        else:
+            raise Exception
+
+
+class TaskOMetaClass:
     def get_input(self):
         print('needs reimplementation')
 
 
-class TaskOClass1(TaskOMetaClass):
-    def get_input(self):
-        return int(input())
-
 if __name__ == '__main__':
-    taskO_1 = TaskOClass1()
-    print(taskO_1.second_max()[1])
+    task_0_obj = TaskOClass1([10, 20, 0])
+    print(second_max(task_0_obj.get_input)[1])
+    task_0_obj = TaskOClass1([10, 20, 20, 0])
+    print(second_max(task_0_obj.get_input)[1])
+    task_0_obj = TaskOClass1([0])
+    print(second_max(task_0_obj.get_input)[1])
