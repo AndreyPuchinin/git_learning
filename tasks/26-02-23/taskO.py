be2d1669e8cad17800564033bf3a5d7b086412da
AndreyPuchinin <78998186+AndreyPuchinin@users.noreply.github.com>
2023-02-28 10:26:27 -0800

    Update taskO.py

--- tasks/26-02-23/taskO.py
+++ tasks/26-02-23/taskO.py
@@ -1,13 +1,17 @@
+from unittest import TestCase, main
+
+
 def get_input():
     return input("enter a number: ")
 
 
-def second_max(get_input_f):
-    v = int(get_input_f())
+
+def second_max():
+    v = int(get_input())
     # print('one')
     if v == 0:
         return 0, 0
-    res = second_max(get_input_f)
+    res = second_max()
     # print('two')
     if v >= res[0]:
         return v, res[0]
@@ -29,15 +33,31 @@ class TaskOClass1:
             raise Exception
 
 
-class TaskOMetaClass:
-    def get_input(self):
-        print('needs reimplementation')
+class TaskOTest(TestCase):
+    def test_1(self):
+        task_o_1 = TaskOClass1([20, 10, 0])
+        global get_input
+        foo1 = get_input
+        get_input = task_o_1.get_input
+        self.assertEqual(second_max()[1], 10)
+        get_input = foo1
+
+    def test_2(self):
+        task_o_1 = TaskOClass1([10, 20, 20, 0])
+        global get_input
+        foo1 = get_input
+        get_input = task_o_1.get_input
+        self.assertEqual(second_max()[1], 20)
+        get_input = foo1
+
+    def test_3(self):
+        task_o_1 = TaskOClass1([0])
+        global get_input
+        foo1 = get_input
+        get_input = task_o_1.get_input
+        self.assertEqual(second_max()[1], 0)
+        get_input = foo1
 
 
 if __name__ == '__main__':
-    task_0_obj = TaskOClass1([10, 20, 0])
-    print(second_max(task_0_obj.get_input)[1])
-    task_0_obj = TaskOClass1([10, 20, 20, 0])
-    print(second_max(task_0_obj.get_input)[1])
-    task_0_obj = TaskOClass1([0])
-    print(second_max(task_0_obj.get_input)[1])
+    main()
